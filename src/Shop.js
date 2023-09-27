b75e5b9cfb45e9f76470ed191bae29e634058677
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-09-27 17:38:03 +0530

    accessing state in redux

--- src/Shop.js
+++ src/Shop.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-// import { bindActionCreators } from 'redux';
+import { bindActionCreators } from 'redux'; 
 import {actionCreators} from './state/index';
 
 const Shop = () => {
   const dispatch = useDispatch();
+  const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators, dispatch);
   return (
     <div>
         <h2>Deposit/Withdraw Money</h2>
-        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
+
+        {/* Using  bindActionCreators*/}
+
+        <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
         Update Balance
-        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button>
+        <button className="btn btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
+
+        {/* without using bindActionCreators */}
+
+        {/* <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
+        Update Balance
+        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button> */}
     </div>
   )
 }
