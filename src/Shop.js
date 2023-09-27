c44b99a84478f64bb42b0f7a069e3bd22a38fd88
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-09-27 17:29:06 +0530

    created redux store

--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+// import { bindActionCreators } from 'redux';
+import {actionCreators} from './state/index';
 
 const Shop = () => {
+  const dispatch = useDispatch();
   return (
     <div>
-        <h2>Buy Nike Shoes 100 USD</h2>
-        <button className="btn btn-primary mx-2">-</button>
-        Add to cart
-        <button className="btn btn-primary mx-2">+</button>
+        <h2>Deposit/Withdraw Money</h2>
+        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
+        Update Balance
+        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button>
     </div>
   )
 }
